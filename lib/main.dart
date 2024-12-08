59efe0e6ccda5a6c99bfdb4d52b4b4cbff75c10c
Adriyan1604 <129313826+Adriyan1604@users.noreply.github.com>
2024-12-08 09:57:12 +0700

    Pesan commit Anda

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:modul_2_public_api/dependency_injection.dart';
+import 'app/routes/app_pages.dart';
+
+Future <void> main() async{
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+    ),
+  );
+  DependencyInjection.init();
+}
